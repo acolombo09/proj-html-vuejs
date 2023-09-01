0bd2a4bf84373eab982e2464ba66e14514ca80be
acolombo09 <acolombo0911@gmail.com>
2023-09-01 17:26:51 +0200

    contact_form_basic_structure-completed

--- src/components/TheMain.vue
+++ src/components/TheMain.vue
@@ -151,6 +151,19 @@ export default {
       </div>
     </div>
 
+    <!-- <div class="divider bg-transparent">
+      <svg width="150" height="100">
+        <defs>
+          <pattern width="150" height="100" id="dotted-texture" patternContentUnits="userSpaceOnUse" patternUnits="userSpaceOnUse">
+            <g id="row-1"><ellipse cx="5" cy="5" rx="3" ry="3" style="stroke:none;stroke-width:0"></ellipse><ellipse cx="20" cy="5" rx="3" ry="3" style="stroke:none;stroke-width:0"></ellipse><ellipse cx="35" cy="5" rx="3" ry="3" style="stroke:none;stroke-width:0"></ellipse><ellipse cx="50" cy="5" rx="3" ry="3" style="stroke:none;stroke-width:0"></ellipse><ellipse cx="65" cy="5" rx="3" ry="3" style="stroke:none;stroke-width:0"></ellipse><ellipse cx="80" cy="5" rx="3" ry="3" style="stroke:none;stroke-width:0"></ellipse><ellipse cx="95" cy="5" rx="3" ry="3" style="stroke:none;stroke-width:0"></ellipse><ellipse cx="110" cy="5" rx="3" ry="3" style="stroke:none;stroke-width:0"></ellipse><ellipse cx="125" cy="5" rx="3" ry="3" style="stroke:none;stroke-width:0"></ellipse><ellipse cx="140" cy="5" rx="3" ry="3" style="stroke:none;stroke-width:0"></ellipse></g>
+            <g id="row-2"><ellipse cx="5" cy="20" rx="3" ry="3" style="stroke:none;stroke-width:0"></ellipse><ellipse cx="20" cy="20" rx="3" ry="3" style="stroke:none;stroke-width:0"></ellipse><ellipse cx="35" cy="20" rx="3" ry="3" style="stroke:none;stroke-width:0"></ellipse><ellipse cx="50" cy="20" rx="3" ry="3" style="stroke:none;stroke-width:0"></ellipse><ellipse cx="65" cy="20" rx="3" ry="3" style="stroke:none;stroke-width:0"></ellipse><ellipse cx="80" cy="20" rx="3" ry="3" style="stroke:none;stroke-width:0"></ellipse><ellipse cx="95" cy="20" rx="3" ry="3" style="stroke:none;stroke-width:0"></ellipse><ellipse cx="110" cy="20" rx="3" ry="3" style="stroke:none;stroke-width:0"></ellipse><ellipse cx="125" cy="20" rx="3" ry="3" style="stroke:none;stroke-width:0"></ellipse><ellipse cx="140" cy="20" rx="3" ry="3" style="stroke:none;stroke-width:0"></ellipse></g>
+            <g id="row-3"><ellipse cx="5" cy="35" rx="3" ry="3" style="stroke:none;stroke-width:0"></ellipse><ellipse cx="20" cy="35" rx="3" ry="3" style="stroke:none;stroke-width:0"></ellipse><ellipse cx="35" cy="35" rx="3" ry="3" style="stroke:none;stroke-width:0"></ellipse><ellipse cx="50" cy="35" rx="3" ry="3" style="stroke:none;stroke-width:0"></ellipse><ellipse cx="65" cy="35" rx="3" ry="3" style="stroke:none;stroke-width:0"></ellipse><ellipse cx="80" cy="35" rx="3" ry="3" style="stroke:none;stroke-width:0"></ellipse><ellipse cx="95" cy="35" rx="3" ry="3" style="stroke:none;stroke-width:0"></ellipse><ellipse cx="110" cy="35" rx="3" ry="3" style="stroke:none;stroke-width:0"></ellipse><ellipse cx="125" cy="35" rx="3" ry="3" style="stroke:none;stroke-width:0"></ellipse><ellipse cx="140" cy="35" rx="3" ry="3" style="stroke:none;stroke-width:0"></ellipse></g>
+            <g id="row-4"><ellipse cx="5" cy="50" rx="3" ry="3" style="stroke:none;stroke-width:0"></ellipse><ellipse cx="20" cy="50" rx="3" ry="3" style="stroke:none;stroke-width:0"></ellipse><ellipse cx="35" cy="50" rx="3" ry="3" style="stroke:none;stroke-width:0"></ellipse><ellipse cx="50" cy="50" rx="3" ry="3" style="stroke:none;stroke-width:0"></ellipse><ellipse cx="65" cy="50" rx="3" ry="3" style="stroke:none;stroke-width:0"></ellipse><ellipse cx="80" cy="50" rx="3" ry="3" style="stroke:none;stroke-width:0"></ellipse><ellipse cx="95" cy="50" rx="3" ry="3" style="stroke:none;stroke-width:0"></ellipse><ellipse cx="110" cy="50" rx="3" ry="3" style="stroke:none;stroke-width:0"></ellipse><ellipse cx="125" cy="50" rx="3" ry="3" style="stroke:none;stroke-width:0"></ellipse><ellipse cx="140" cy="50" rx="3" ry="3" style="stroke:none;stroke-width:0"></ellipse></g>
+          </pattern>
+        </defs><rect width="150" height="100" x="0" y="0" fill="url(#dotted-texture)"></rect>
+      </svg>
+    </div> -->
+
     <!-- About section The Company -->
     <div class="px-4 py-5 bg-black">
       <div class="col-md-9 mx-auto">
@@ -272,6 +285,110 @@ export default {
       </div>
     </div>
 
+    <!-- Contact Section -->
+    <div class="px-4 py-5 bg-secondary">
+      <div class="col-md-9 mx-auto">
+        <div class="row justify-content-center">
+          <div class="col-md-6 text-center mb-5">
+            <h2 class="heading-section">Contact Form #01</h2>
+          </div>
+        </div>
+        <div class="row justify-content-center">
+          <div class="col-lg-10 col-md-12">
+            <div class="wrapper">
+              <div class="row no-gutters">
+                <div class="col-md-7 d-flex align-items-stretch">
+                  <div class="contact-wrap w-100 p-md-5 p-4">
+                    <h3 class="mb-4">Get in touch</h3>
+                    <div id="form-message-warning" class="mb-4"></div>
+                    <div id="form-message-success" class="mb-4">Your message was sent, thank you!</div>
+                    <form method="POST" id="contactForm" name="contactForm" novalidate="novalidate">
+                      <div class="row">
+                        <div class="col-md-6">
+                          <div class="form-group">
+                          <input type="text" class="form-control" name="name" id="name" placeholder="Name">
+                          </div>
+                        </div>
+                        <div class="col-md-6">
+                          <div class="form-group">
+                            <input type="email" class="form-control" name="email" id="email" placeholder="Email">
+                          </div>
+                        </div>
+                        <div class="col-md-12">
+                          <div class="form-group">
+                            <input type="text" class="form-control" name="subject" id="subject" placeholder="Subject">
+                          </div>
+                        </div>
+                        <div class="col-md-12">
+                          <div class="form-group">
+                            <textarea name="message" class="form-control" id="message" cols="30" rows="7" placeholder="Message"></textarea>
+                          </div>
+                        </div>
+                        <div class="col-md-12">
+                          <div class="form-group">
+                            <input type="submit" value="Send Message" class="btn btn-primary">
+                            <div class="submitting"></div>
+                          </div>
+                        </div>
+                      </div>
+                    </form>
+                  </div>
+                </div>
+                <div class="col-md-5 d-flex align-items-stretch">
+                  <div class="info-wrap w-100 p-lg-5 p-4">
+                    <h3 class="mb-4 mt-md-4">Contact us</h3>
+                    <div class="dbox w-100 d-flex align-items-start">
+                      <div class="icon d-flex align-items-center justify-content-center">
+                        <span class="fa fa-map-marker"></span>
+                      </div>
+                      <div class="text pl-3">
+                        <p>
+                          <span>Address:</span>
+                          198 West 21th Street, Suite 721 New York NY 10016
+                        </p>
+                      </div>
+                    </div>
+                    <div class="dbox w-100 d-flex align-items-center">
+                      <div class="icon d-flex align-items-center justify-content-center">
+                        <span class="fa fa-phone"></span>
+                      </div>
+                      <div class="text pl-3">
+                        <p>
+                          <span>Phone:</span>
+                          <a href="tel://1234567920">+ 1235 2355 98</a>
+                        </p>
+                      </div>
+                    </div>
+                    <div class="dbox w-100 d-flex align-items-center">
+                      <div class="icon d-flex align-items-center justify-content-center">
+                        <span class="fa fa-paper-plane"></span>
+                      </div>
+                      <div class="text pl-3">
+                        <p>
+                          <span>Email:</span> 
+                          <a href="mailto:info@yoursite.com">info@yoursite.com</a>
+                        </p>
+                      </div>
+                    </div>
+                    <div class="dbox w-100 d-flex align-items-center">
+                      <div class="icon d-flex align-items-center justify-content-center">
+                        <span class="fa fa-globe"></span>
+                      </div>
+                      <div class="text pl-3">
+                        <p>
+                          <span>Website</span>
+                          <a href="#">yoursite.com</a>
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </main>
 </template>
 
