e0d70071853b449803dcb0ed93173e8f96aa58d7
acolombo09 <acolombo0911@gmail.com>
2023-09-02 13:48:54 +0200

    result&sponsors_basic_structure-completed

--- src/components/TheMain.vue
+++ src/components/TheMain.vue
@@ -389,6 +389,60 @@ export default {
         </div>
       </div>
     </div>
+
+    <!-- Results Section -->
+    <div class="px-4 py-5" id="icon-grid">
+      <div class="col-md-9 mx-auto text-center">
+        <h5 class="pb-2">WHAT ARE WE DOING</h5>
+        <h2 class="pb-2">Results in Numbers</h2>
+
+        <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-4 g-4 py-5">
+          <div class="col d-flex align-items-start">
+            <svg class="bi text-body-secondary flex-shrink-0 me-3" width="1.75em" height="1.75em"><use xlink:href="#bootstrap"></use></svg>
+            <div>
+              <h3 class="fw-bold mb-0 fs-4 text-body-emphasis">128</h3>
+              <p>Certifications</p>
+            </div>
+          </div>
+          <div class="col d-flex align-items-start">
+            <svg class="bi text-body-secondary flex-shrink-0 me-3" width="1.75em" height="1.75em"><use xlink:href="#cpu-fill"></use></svg>
+            <div>
+              <h3 class="fw-bold mb-0 fs-4 text-body-emphasis">230</h3>
+              <p>Employees</p>
+            </div>
+          </div>
+          <div class="col d-flex align-items-start">
+            <svg class="bi text-body-secondary flex-shrink-0 me-3" width="1.75em" height="1.75em"><use xlink:href="#calendar3"></use></svg>
+            <div>
+              <h3 class="fw-bold mb-0 fs-4 text-body-emphasis">517</h3>
+              <p>Customers</p>
+            </div>
+          </div>
+          <div class="col d-flex align-items-start">
+            <svg class="bi text-body-secondary flex-shrink-0 me-3" width="1.75em" height="1.75em"><use xlink:href="#home"></use></svg>
+            <div>
+              <h3 class="fw-bold mb-0 fs-4 text-body-emphasis">94</h3>
+              <p>Countries Served</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- Sponsor div IN Results section -->
+    <div class="px-4 py-5 bg-black sponsor-div">
+      <div class="container">
+        <footer class="py-3 my-4">
+          <ul class="nav justify-content-center">
+            <li class="nav-item"><a href="#" class="nav-link px-2 text-secondary">jquery</a></li>
+            <li class="nav-item"><a href="#" class="nav-link px-2 text-secondary">less</a></li>
+            <li class="nav-item"><a href="#" class="nav-link px-2 text-secondary">woocommerce</a></li>
+            <li class="nav-item"><a href="#" class="nav-link px-2 text-secondary">wordpress</a></li>
+            <li class="nav-item"><a href="#" class="nav-link px-2 text-secondary">pingdom</a></li>
+            <li class="nav-item"><a href="#" class="nav-link px-2 text-secondary">jquery</a></li>
+          </ul>
+        </footer>
+      </div>
+    </div>
   </main>
 </template>
 
