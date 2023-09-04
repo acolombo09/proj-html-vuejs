85c8ce919e651f271f288245d2f40f31f416773f
acolombo09 <acolombo0911@gmail.com>
2023-09-04 12:55:36 +0200

    contact_section_completed

--- src/components/ContactSection.vue
+++ src/components/ContactSection.vue
@@ -11,47 +11,52 @@ export default {
 
 <template>
   <!-- Contact Section -->
-  <div class="px-4 py-5 bg-secondary">
+  <div class="px-4 py-5">
     <div class="col-md-9 mx-auto">
-      <div class="row justify-content-center">
-        <div class="col-md-6 text-center mb-5">
-          <h2 class="heading-section">Contact Form #01</h2>
-        </div>
-      </div>
       <div class="row justify-content-center">
         <div class="col-lg-10 col-md-12">
           <div class="wrapper">
             <div class="row no-gutters">
-              <div class="col-md-7 d-flex align-items-stretch">
-                <div class="contact-wrap w-100 p-md-5 p-4">
-                  <h3 class="mb-4">Get in touch</h3>
-                  <div id="form-message-warning" class="mb-4"></div>
-                  <div id="form-message-success" class="mb-4">Your message was sent, thank you!</div>
+              <div class="col-md-7 d-flex">
+                <div class="contact-wrap w-100 p-5">
+                  <div class="mb-4">
+                    <h6 class="text-success mb-3 fw-bold">SEND A MESSAGE</h6>
+                    <h2 class="mb-4 fw-bold">Get in Touch</h2>
+                    <p class="text-secondary">
+                      We will respond to your message as soon as possible.
+                    </p>
+                  </div>
+
                   <form method="POST" id="contactForm" name="contactForm" novalidate="novalidate">
-                    <div class="row">
-                      <div class="col-md-6">
+                    <div class="row mt-5">
+                      <div class="col-md-6 my-2">
                         <div class="form-group">
-                        <input type="text" class="form-control" name="name" id="name" placeholder="Name">
+                        <input type="text" class="form-control rounded-1" name="name" id="name" placeholder="Name">
                         </div>
                       </div>
-                      <div class="col-md-6">
+                      <div class="col-md-6 my-2">
                         <div class="form-group">
-                          <input type="email" class="form-control" name="email" id="email" placeholder="Email">
+                          <input type="email" class="form-control rounded-1" name="email" id="email" placeholder="Email">
                         </div>
                       </div>
-                      <div class="col-md-12">
+                      <div class="col-md-6 my-2">
                         <div class="form-group">
-                          <input type="text" class="form-control" name="subject" id="subject" placeholder="Subject">
+                          <input type="text" class="form-control rounded-1" name="phone" id="phone" placeholder="Phone">
                         </div>
                       </div>
-                      <div class="col-md-12">
+                      <div class="col-md-6 my-2">
                         <div class="form-group">
-                          <textarea name="message" class="form-control" id="message" cols="30" rows="7" placeholder="Message"></textarea>
+                          <input type="text" class="form-control rounded-1" name="moreinfo" id="moreinfo" placeholder="More Info">
                         </div>
                       </div>
-                      <div class="col-md-12">
+                      <div class="col-md-12 my-2">
                         <div class="form-group">
-                          <input type="submit" value="Send Message" class="btn btn-primary">
+                          <textarea name="message" class="form-control rounded-1" id="message" cols="30" rows="7" placeholder="Message"></textarea>
+                        </div>
+                      </div>
+                      <div class="col-md-12 my-2">
+                        <div class="form-group">
+                          <input type="submit" value="SEND" class="btn btn-success rounded-1">
                           <div class="submitting"></div>
                         </div>
                       </div>
@@ -59,52 +64,50 @@ export default {
                   </form>
                 </div>
               </div>
-              <div class="col-md-5 d-flex align-items-stretch">
-                <div class="info-wrap w-100 p-lg-5 p-4">
-                  <h3 class="mb-4 mt-md-4">Contact us</h3>
-                  <div class="dbox w-100 d-flex align-items-start">
+              <div class="col-md-5 d-flex">
+                <div class="info-wrap w-100 p-5">
+                  <div class="mb-3">
+                    <h3 class="mb-3 fw-bold">Example Inc.</h3>
+                    <p class="text-secondary mb-4">
+                      Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    </p>
+                    <p class="text-secondary mb-5">
+                      Praesent diam lacus, diapibus sed imperdiet consectetur.
+                    </p>
+                  </div>
+                  
+                  <div class="dbox w-100 d-flex align-items-start my-3">
                     <div class="icon d-flex align-items-center justify-content-center">
-                      <span class="fa fa-map-marker"></span>
+                      <i class="fa-solid fa-phone"></i>
                     </div>
-                    <div class="text pl-3">
-                      <p>
-                        <span>Address:</span>
-                        198 West 21th Street, Suite 721 New York NY 10016
-                      </p>
+                    <div class="ms-4">
+                      <a href="#">
+                        <p class="d-inline-block text-success">+1 (305) 1234-5678</p>
+                      </a>
                     </div>
                   </div>
-                  <div class="dbox w-100 d-flex align-items-center">
+                  <div class="dbox w-100 d-flex align-items-start my-3">
                     <div class="icon d-flex align-items-center justify-content-center">
-                      <span class="fa fa-phone"></span>
+                      <i class="fa-solid fa-envelope"></i>
                     </div>
-                    <div class="text pl-3">
-                      <p>
-                        <span>Phone:</span>
-                        <a href="tel://1234567920">+ 1235 2355 98</a>
-                      </p>
+                    <div class="ms-4">
+                      <a href="#">
+                        <p class="d-inline-block text-success">hello@example.com</p>
+                      </a>
                     </div>
                   </div>
-                  <div class="dbox w-100 d-flex align-items-center">
+                  <div class="dbox w-100 d-flex align-items-start my-3">
                     <div class="icon d-flex align-items-center justify-content-center">
-                      <span class="fa fa-paper-plane"></span>
+                      <i class="fa-solid fa-location-dot"></i>
                     </div>
-                    <div class="text pl-3">
-                      <p>
-                        <span>Email:</span> 
-                        <a href="mailto:info@yoursite.com">info@yoursite.com</a>
-                      </p>
+                    <div class="ms-4">
+                      <a href="#">
+                        <p class="d-inline-block text-success">Main Avenue, 987</p>
+                      </a>
                     </div>
                   </div>
-                  <div class="dbox w-100 d-flex align-items-center">
-                    <div class="icon d-flex align-items-center justify-content-center">
-                      <span class="fa fa-globe"></span>
-                    </div>
-                    <div class="text pl-3">
-                      <p>
-                        <span>Website</span>
-                        <a href="#">yoursite.com</a>
-                      </p>
-                    </div>
+                  <div class="dbox w-100 d-flex align-items-start my-3">
+                    <button type="button" class="btn btn-outline-success px-4 rounded-1">VIEW MAP</button>
                   </div>
                 </div>
               </div>
@@ -120,6 +123,8 @@ export default {
 
 @use "../styles/partials/variables" as *;
 
-
+.form-control {
+  background-color: $form-bg-color;
+}
 
 </style>
